6eb33fc6eb5710ac27b2c5dec46e93b8f3eafd8d
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-04-11 21:03:10 +0900

    added initial landing page (by Alessandro)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,35 @@
-export default function Page() {
-    return <p>landing page</p>
+import styles from "@/components/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href=""> Resume Generator </a>{" "}
+        </h1>
+
+        <p className={styles.description}>
+          Easily create professional resumes with our resume builder.{" "}
+        </p>
+
+        <div className={styles.grid}>
+          <a href="#" className={styles.card}>
+            <h3> Create a Resume & rarr; </h3>{" "}
+            <p> Build your resume from one of the following templates. </p>{" "}
+          </a>
+          <a href="#" className={styles.card}>
+            <h3> Download & Share & rarr; </h3>{" "}
+            <p>
+              {" "}
+              Download your resume in PDF format and share it with employers.{" "}
+            </p>{" "}
+          </a>{" "}
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p> ©2024 Resume Generator.All rights reserved. </p>{" "}
+      </footer>
+    </div>
+  );
 }
