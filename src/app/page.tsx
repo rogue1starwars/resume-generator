0e06d0f0b3cdfd7c73ee292d80c2932b4eda4197
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-04-14 19:53:31 +0900

    changed a tag to Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,31 @@
 import styles from "@/components/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href=""> Resume Generator </a>
+          Welcome to <Link href=""> Resume Generator </Link>
         </h1>
 
         <p className={styles.description}>
-          Easily create professional resumes with our resume builder.{" "}
+          Easily create professional resumes with our resume builder.
         </p>
 
         <div className={styles.grid}>
-          <a href="#" className={styles.card}>
-            <h3> Create a Resume & rarr; </h3>{" "}
-            <p> Build your resume from one of the following templates. </p>{" "}
-          </a>
-          <a href="#" className={styles.card}>
-            <h3> Create resume without logging in</h3>{" "}
-          </a>{" "}
+          <Link href="/home" className={styles.card}>
+            <h3> Create Link Resume; </h3>
+            <p> Build your resume from one of the following templates. </p>
+          </Link>
+          <Link href="/home" className={styles.card}>
+            <h3> Create resume without logging in</h3>
+          </Link>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <p> ©2024 Resume Generator.All rights reserved. </p>{" "}
+        <p> ©2024 Resume Generator.All rights reserved. </p>
       </footer>
     </div>
   );
